06c722df606d6b2d1bf7cdb6bb2888717ab9c96a
Kostiantyn Hantsov <k.gantsov@gmail.com>
2018-09-24 08:12:14 +0300

    Basic implementation of RateLimiter struct

---
+++ pkg/limiter/limiter.go
@@ -0,0 +1,83 @@
+package limiter
+
+import (
+	"fmt"
+	"math"
+	"sync"
+	"sync/atomic"
+	"time"
+)
+
+type Bucket struct {
+	Value        int64
+	MaxTokens    int64
+	RefillTime   int64
+	RefillAmount int64
+	LastUpdate   int64
+}
+
+type RateLimiter struct {
+	Buckets map[string]*Bucket
+	mu      sync.RWMutex
+}
+
+func NewRateLimiter() *RateLimiter {
+	rateLimiter := new(RateLimiter)
+	rateLimiter.Buckets = make(map[string]*Bucket)
+
+	return rateLimiter
+}
+
+func (l *RateLimiter) AddLimiter(key string, max_tokens int64, refill_time int64, refill_amount int64) {
+	l.mu.Lock()
+	l.Buckets[key] = &Bucket{
+		Value:        max_tokens,
+		MaxTokens:    max_tokens,
+		RefillTime:   refill_time,
+		RefillAmount: refill_amount,
+		LastUpdate:   time.Now().Unix(),
+	}
+	l.mu.Unlock()
+}
+
+func (l *RateLimiter) Reduce(key string, tokens int64) (int64, error) {
+	l.mu.RLock()
+	bucket, ok := l.Buckets[key]
+	l.mu.Unlock()
+
+	if !ok {
+		return 0, fmt.Errorf("Bucket `%s` does not exist", key)
+	}
+
+	now := time.Now().Unix()
+	refillCount := math.Floor(
+		float64(now-bucket.LastUpdate) / float64(bucket.RefillTime),
+	)
+
+	atomic.StoreInt64(
+		&bucket.Value,
+		int64(math.Min(
+			float64(bucket.MaxTokens),
+			float64(bucket.Value)+(refillCount*float64(bucket.RefillAmount)),
+		)),
+	)
+
+	atomic.StoreInt64(
+		&bucket.LastUpdate,
+		int64(math.Min(
+			float64(now),
+			float64(bucket.LastUpdate)+refillCount*float64(bucket.LastUpdate),
+		)),
+	)
+
+	if tokens > bucket.Value {
+		return -1, nil
+	}
+
+	atomic.AddInt64(
+		&bucket.Value,
+		-tokens,
+	)
+
+	return bucket.Value, nil
+}
