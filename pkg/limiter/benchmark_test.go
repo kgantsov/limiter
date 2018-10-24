bf8db6136f41e7478926e3f0bfa348905bd34820
Kostiantyn Hantsov <k.gantsov@gmail.com>
2018-10-24 21:26:19 +0200

    Improve benchmarks

--- pkg/limiter/benchmark_test.go
+++ pkg/limiter/benchmark_test.go
@@ -1,16 +1,24 @@
 package limiter
 
 import (
+	"fmt"
+	"math/rand"
 	"testing"
+	"time"
 )
 
-func benchmarkReduce(key string, maxTokens int64, refillTime int64, refillAmount int64, tokens int64, b *testing.B) {
+func benchmarkReduce(numberOfKeys int, maxTokens int64, refillTime int64, refillAmount int64, tokens int64, b *testing.B) {
 	b.StopTimer()
 	rl := NewRateLimiter()
+
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
 
 	b.ResetTimer()
 
 	for i := 0; i < b.N; i++ {
+		index := r.Int31n(int32(numberOfKeys))
+		key := fmt.Sprintf("user:%d", index)
+
 		b.StartTimer()
 
 		rl.Reduce(key, maxTokens, refillTime, refillAmount, tokens)
@@ -20,5 +28,5 @@ func benchmarkReduce(key string, maxTokens int64, refillTime int64, refillAmount
 }
 
 func BenchmarkReduce_100_1000(b *testing.B) {
-	benchmarkReduce("user:1001", 1000, 1, 1000, 1, b)
+	benchmarkReduce(100, 1000, 1, 1000, 1, b)
 }
