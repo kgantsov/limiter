d74ea0af08afdc1b427c13e0629267a61acab6c4
Kostiantyn Hantsov <k.gantsov@gmail.com>
2021-01-22 18:47:59 +0100

    Improve benchmarks

--- README.md
+++ README.md
@@ -31,11 +31,17 @@ Limiter is a rate limiter application that has http and redis interfaces
 
 ### Benchmarks
 
-    > go test -run XXX -bench .
+    > $ go test -cpu 1,2,4,8 -run XXX -bench .
     goos: darwin
     goarch: amd64
     pkg: github.com/kgantsov/limiter/pkg/limiter
-    BenchmarkReduce_100_1000_1_1000_1-4          	 1000000	      1778 ns/op	      16 B/op	       2 allocs/op
-    BenchmarkReduce_10000000_1000_10_1000_10-4   	  500000	      2557 ns/op	     207 B/op	       2 allocs/op
+    BenchmarkReduce_100_1000_1_1000_1            	 2734095	       446 ns/op
+    BenchmarkReduce_100_1000_1_1000_1-2          	 2373958	       515 ns/op
+    BenchmarkReduce_100_1000_1_1000_1-4          	 2350014	       504 ns/op
+    BenchmarkReduce_100_1000_1_1000_1-8          	 2367374	       500 ns/op
+    BenchmarkReduce_10000000_1000_10_1000_10     	 1548418	       717 ns/op
+    BenchmarkReduce_10000000_1000_10_1000_10-2   	 1000000	      1020 ns/op
+    BenchmarkReduce_10000000_1000_10_1000_10-4   	 1222263	       939 ns/op
+    BenchmarkReduce_10000000_1000_10_1000_10-8   	 1375906	       775 ns/op
     PASS
-    ok  	github.com/kgantsov/limiter/pkg/limiter	62.610s
+    ok  	github.com/kgantsov/limiter/pkg/limiter	14.058s
--- pkg/limiter/benchmark_test.go
+++ pkg/limiter/benchmark_test.go
@@ -7,29 +7,29 @@ import (
 	"time"
 )
 
-func benchmarkReduce(numberOfKeys int, maxTokens int64, refillTime int64, refillAmount int64, tokens int64, b *testing.B) {
-	b.StopTimer()
+func BenchmarkReduce_100_1000_1_1000_1(b *testing.B) {
+	numberOfKeys := 1000
 	rl := NewRateLimiter()
 
-	r := rand.New(rand.NewSource(time.Now().UnixNano()))
-
-	b.ResetTimer()
-
-	for i := 0; i < b.N; i++ {
-		index := r.Int31n(int32(numberOfKeys))
-		key := fmt.Sprintf("user:%d", index)
-
-		b.StartTimer()
-
-		rl.Reduce(key, maxTokens, refillTime, refillAmount, tokens)
-
-		b.StopTimer()
-	}
-}
-
-func BenchmarkReduce_100_1000_1_1000_1(b *testing.B) {
-	benchmarkReduce(100, 1000, 1, 1000, 1, b)
+	b.RunParallel(func(pb *testing.PB) {
+		r := rand.New(rand.NewSource(time.Now().UnixNano()))
+		for pb.Next() {
+			index := r.Int31n(int32(numberOfKeys))
+			key := fmt.Sprintf("user:%d", index)
+			rl.Reduce(key, 1000, 1, 1000, 1)
+		}
+	})
 }
 func BenchmarkReduce_10000000_1000_10_1000_10(b *testing.B) {
-	benchmarkReduce(10000000, 1000, 10, 1000, 10, b)
+	numberOfKeys := 10000000
+	rl := NewRateLimiter()
+
+	b.RunParallel(func(pb *testing.PB) {
+		r := rand.New(rand.NewSource(time.Now().UnixNano()))
+		for pb.Next() {
+			index := r.Int31n(int32(numberOfKeys))
+			key := fmt.Sprintf("user:%d", index)
+			rl.Reduce(key, 1000, 10, 1000, 10)
+		}
+	})
 }
